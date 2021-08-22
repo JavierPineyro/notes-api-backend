48fecc3c09451ab632fe206a55c54553324c4542
JavierPineyro <javidunam22@gmail.com>
2021-08-22 19:03:15 -0300

    Heroku deploy

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use((request, response, next) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 // Listen es Asincrono, le pasamos un callback que se ejecuta solo cuando ya se
 // levanto el server
 app.listen(PORT, () => {
